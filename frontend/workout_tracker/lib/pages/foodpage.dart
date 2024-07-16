dca59e60aa773250973c589b0b3a6ac20d5767c2
Aryaman Dubey <aryaman.dubey@kcl.ac.uk>
2024-07-16 11:15:48 +0100

    add placeholder structure to flutter code

---
+++ frontend/workout_tracker/lib/pages/foodpage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CalorieTracking extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Food Tracking Page',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
