df59c43817696bfcd6cd3fa4293277baf920ffad
Aryaman Dubey <aryaman.dubey@kcl.ac.uk>
2024-07-19 16:15:57 +0100

    fix issue of workout generation

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,34 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy import ForeignKey, insert, select
+from sqlalchemy import String, Integer, create_engine, Table, Column, Date
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, Session
 import random
-
-
-# Initial REST API that executes only core functionality
+from datetime import date
 
 app = Flask(__name__)
+
+# Configuration for SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./workouts.db'
+db = SQLAlchemy(app)
+
+
+# Define the Workout model
+class Workout(db.Model):
+    __tablename__ = "workouts"
+    id = mapped_column(Integer, primary_key=True)
+    date = mapped_column(Date, nullable=False, unique=True)
+    pushup = mapped_column(String(80), nullable=False)
+    pullup = mapped_column(String(80), nullable=False)
+    squat = mapped_column(String(80), nullable=False)
+
+    def __repr__(self):
+        return f"<Workout(id={self.id}, date={self.date}, pushup={self.pushup}, pullup={self.pullup}, squat={self.squat})>"
+
+# Create the database tables
+with app.app_context():
+    db.create_all()
 
 # Arrays of exercises
 pushup_exercises = [
@@ -35,8 +59,29 @@ def generate_random_workout():
 
 @app.route('/workout', methods=['GET'])
 def get_workout():
-    workout = generate_random_workout()
-    return jsonify(workout)
+    today = date.today()
+    workout = db.session.execute(select(Workout).filter_by(date=today)).scalar_one_or_none()
+    if workout:
+        response = {
+            "pushup": workout.pushup,
+            "pullup": workout.pullup,
+            "squat_lunge": workout.squat
+        }
+        print(f"Workout exists in database already -> {response}")
+    else:
+        random_workout = generate_random_workout()
+        print(f"workout does not exist, saving {random_workout}")
+        workout = Workout(
+            date=today,
+            pushup=random_workout["pushup"],
+            pullup=random_workout["pullup"],
+            squat=random_workout["squat_lunge"]
+        ) # type: ignore
+        db.session.add(workout)
+        db.session.commit()
+        response = random_workout
+        print(f"response = {jsonify(response)}")
+    return jsonify(response)
 
 @app.route('/')
 def home():
--- backend/models.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-
-# def create_app():
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./workouts.db'
-#
