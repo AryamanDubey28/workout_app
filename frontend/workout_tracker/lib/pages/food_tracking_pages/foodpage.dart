0aa3baade009f3440b8785af77a2168c1340dcf8
Aryaman Dubey <aryaman.dubey@kcl.ac.uk>
2024-07-29 15:04:57 +0100

    add date tiles on foodpage

--- frontend/workout_tracker/lib/pages/food_tracking_pages/foodpage.dart
+++ frontend/workout_tracker/lib/pages/food_tracking_pages/foodpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:workout_tracker/pages/food_tracking_pages/food_entry.dart';
 import 'package:workout_tracker/pages/food_tracking_pages/nutritional_summary.dart';
 import 'package:workout_tracker/pages/food_tracking_pages/water_tracking.dart';
@@ -8,38 +9,104 @@ class CalorieTracking extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    // Determine the current day of the week and set up date formatting
+    final DateTime today = DateTime.now();
+    final DateFormat dayFormatter =
+        DateFormat('EEE'); // Format for day abbreviations
+    final DateFormat numericDayFormatter =
+        DateFormat('d'); // Format for numeric day
+    final List<DateTime> weekDays = List.generate(
+        5,
+        (index) => DateTime(
+            today.year, today.month, today.day + index - today.weekday + 1));
+
+    return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
           child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                NutritionalSummary(),
-                Divider(
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('Weekdays',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    IconButton(
+                      icon: const Icon(Icons.calendar_today),
+                      onPressed: () {}, // Calendar icon action
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 70,
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: weekDays.length,
+                    separatorBuilder: (context, index) =>
+                        const SizedBox(width: 8),
+                    itemBuilder: (context, index) {
+                      bool isToday = weekDays[index].day == today.day;
+                      return Card(
+                        elevation: 2,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          side: BorderSide(
+                              color: isToday ? Colors.blue : Colors.transparent,
+                              width: 2),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 8),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                numericDayFormatter.format(weekDays[index]),
+                                style: TextStyle(
+                                    color: isToday ? Colors.blue : Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                dayFormatter.format(weekDays[index]),
+                                style: TextStyle(
+                                    color:
+                                        isToday ? Colors.blue : Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const NutritionalSummary(),
+                const Divider(
                   color: Colors.grey,
                   thickness: 1,
                   indent: 10,
                   endIndent: 10,
                 ),
-                FoodEntrySection(
+                const FoodEntrySection(
                   title: 'Breakfast',
                   calories: '272 kcal',
                   imagePath: 'assets/images/fruits.png',
                 ),
-                FoodEntrySection(
+                const FoodEntrySection(
                   title: 'Lunch',
                   calories: '477 kcal',
                   imagePath: 'assets/images/lunch-bag.png',
                 ),
-                FoodEntrySection(
+                const FoodEntrySection(
                   title: 'Dinner',
                   calories: '650 kcal',
                   imagePath: 'assets/images/dinner.png',
                 ),
-                SizedBox(height: 2),
-                WaterTrackingSection(),
+                const SizedBox(height: 2),
+                const WaterTrackingSection(),
               ],
             ),
           ),
